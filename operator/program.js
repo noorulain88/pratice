ab592c4510253e277362deb132a7d11a4c96cb88
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-20 19:24:00 +0500

    pratice

--- operator/program.js
+++ operator/program.js
@@ -14,3 +14,31 @@ var u=7
 var y=++u
 console.log("u" ,u)
 console.log("y" ,y)
+// var 
+// string
+var subject="math and ..."
+console.log ("my favourite subject is ",subject) 
+var food="biryani and ..."
+console.log ("my favourite food is ",food) 
+var vegetable="carrot and ..."
+console.log ("my favourite vegatable is ",vegetable)
+// boolean
+var hafiz=true
+console.log ("she is hafiz  ",hafiz)
+var student=false
+console.log ("she is student ",student)
+var developers=false
+console.log ("she is developers ",developers)
+// numbers
+var even=2
+console.log("even numeber start with ",even)
+var odd=1
+console.log("odd numeber start with ",odd)
+// operater 
+// additiion
+
+
+
+
+
+
