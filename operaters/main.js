a083c92dd7862fcc31c264148afc52485ba4e981
noorulain88 <noorulainnoorulain86@gmail.com>
2025-10-31 20:09:41 +0500

    swithch and nested if

---
+++ operaters/main.js
@@ -0,0 +1,73 @@
+console.log("hello world");
+
+// if and else condition
+var Name=prompt(
+"enter your name"
+)
+if(Name=="noor"){
+    console.log("user name is noor")
+}
+else{
+    
+    console.log("user name is not noor")
+}
+// if else and else if 
+var age=prompt("enter ur age ")
+if(age<18){
+    console.log("you are welcome")
+ 
+}
+else if (age==18){
+    console.log("you are 18 Years old")
+}else if (age<20){
+    console.log("Now you are not teenager")
+}else if (age<25){
+    console.log("you are 25  Years old you are young")
+}
+else{
+    console.log("you are not grater then 18")
+}
+// nested if 
+var marks=prompt("enter your marks ")
+if(marks>=90){
+console.log("your grade is A")
+if(marks>=80){
+    console.log("your grade is A+")
+}
+if(marks>=50){
+    console.log("your grade is B")
+}
+if(marks>=50){
+    console.log("your grade is C")
+}
+if(marks>=40){
+    console.log("your grade is  you are fail")
+}
+
+}
+
+// swithch case
+
+var firstName=parseFloat(prompt("enter first number"))
+var secondName=parseFloat(prompt("enter second number"))
+var opeater=prompt("enter operater +,_,*,/")
+
+switch(opeater){
+    case "+":
+        result=firstName+secondName
+        console.log("addition of first number and seconde num is "+ result)
+        break;
+        case "_":
+            result=firstName-secondName
+            console.log("subtraction  of first number and seconde num is" - result)
+            break;
+            case "*":
+                result=firstName*secondName
+                console.log("multiplication of first number and seconde number is " *result)
+                break;
+                case"/":
+                   result=firstName/secondName
+                console.log("division  of first number and seconde number is " /result)
+                break;
+
+}
--- operaters/program.js
+++ operaters/program.js
@@ -1,46 +1,46 @@
 console.log("Hello world!");
 
 // program to check user name
-var username=prompt("enter your name");
-if(username=="noor"){
-    console.log("welome noor !")
-}
-else{
-    console.log("welcome guset !")
-}
+// var username=prompt("enter your name");
+// if(username=="noor"){
+//     console.log("welome noor !")
+// }
+// else{
+//     console.log("welcome guset !")
+// }
 
 // program to check trip premission
-var trip=prompt("You want to go trip ?yes/no");
-if(trip=="yes"){
-    console.log("enjoy your trip")
-}
-else{
-    console.log("stay home and enjoy sleeping")
-}
+// var trip=prompt("You want to go trip ?yes/no");
+// if(trip=="yes"){
+//     console.log("enjoy your trip")
+// }
+// else{
+//     console.log("stay home and enjoy sleeping")
+// }
 
 // program to check weather
-var weather=prompt("is it raining outside ? yes or no")
-if(weather=="yes"){
-    console.log("enojy rainy day")
-}
-else{
-    console.log("wait for rainy day")
-}
+// var weather=prompt("is it raining outside ? yes or no")
+// if(weather=="yes"){
+//     console.log("enojy rainy day")
+// }
+// else{
+//     console.log("wait for rainy day")
+// }
 // program to check their study time
-var study= prompt(parseInt ("enter your study time "))
-if(study=="6"){
-    console.log("good job , keep it up")
-}
-else{(study<6)
-    console.log(" you need to study more" )
-}
+// var study= prompt(parseInt ("enter your study time "))
+// if(study=="6"){
+//     console.log("good job , keep it up")
+// }
+// else{(study<6)
+//     console.log(" you need to study more" )
+// }
 
 // program to check your daily routine
-var routine=prompt("do you have a daily routine ? yes/no")
-if(routine=="yes"){
-    console.log("good to hear that")
-}
-else{
-    console.log("try to make a daily routine")
-}
+// var routine=prompt("do you have a daily routine ? yes/no")
+// if(routine=="yes"){
+//     console.log("good to hear that")
+// }
+// else{
+//     console.log("try to make a daily routine")
+// }
 
