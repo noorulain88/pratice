63117db330f2f1fe8f13341e38eb594ee380a3ac
noorulain88 <noorulainnoorulain86@gmail.com>
2025-11-01 18:30:53 +0500

    loop

--- operaters/main.js
+++ operaters/main.js
@@ -1,73 +1,121 @@
 console.log("hello world");
 
 // if and else condition
-var Name=prompt(
-"enter your name"
-)
-if(Name=="noor"){
-    console.log("user name is noor")
-}
-else{
+// var Name=prompt(
+// "enter your name"
+// )
+// if(Name=="noor"){
+//     console.log("user name is noor")
+// }
+// else{
     
-    console.log("user name is not noor")
-}
+//     console.log("user name is not noor")
+// }
 // if else and else if 
-var age=prompt("enter ur age ")
-if(age<18){
-    console.log("you are welcome")
+// var age=prompt("enter ur age ")
+// if(age<18){
+//     console.log("you are welcome")
  
-}
-else if (age==18){
-    console.log("you are 18 Years old")
-}else if (age<20){
-    console.log("Now you are not teenager")
-}else if (age<25){
-    console.log("you are 25  Years old you are young")
-}
-else{
-    console.log("you are not grater then 18")
-}
+// }
+// else if (age==18){
+//     console.log("you are 18 Years old")
+// }else if (age<20){
+//     console.log("Now you are not teenager")
+// }else if (age<25){
+//     console.log("you are 25  Years old you are young")
+// }
+// else{
+//     console.log("you are not grater then 18")
+// }
 // nested if 
-var marks=prompt("enter your marks ")
-if(marks>=90){
-console.log("your grade is A")
-if(marks>=80){
-    console.log("your grade is A+")
-}
-if(marks>=50){
-    console.log("your grade is B")
-}
-if(marks>=50){
-    console.log("your grade is C")
-}
-if(marks>=40){
-    console.log("your grade is  you are fail")
-}
+// var marks=prompt("enter your marks ")
+// if(marks>=90){
+// console.log("your grade is A")
+// if(marks>=80){
+//     console.log("your grade is A+")
+// }
+// if(marks>=50){
+//     console.log("your grade is B")
+// }
+// if(marks>=50){
+//     console.log("your grade is C")
+// }
+// if(marks>=40){
+//     console.log("your grade is  you are fail")
+// }
 
-}
+// }
 
 // swithch case
 
-var firstName=parseFloat(prompt("enter first number"))
-var secondName=parseFloat(prompt("enter second number"))
-var opeater=prompt("enter operater +,_,*,/")
+// var firstName=parseFloat(prompt("enter first number"))
+// var secondName=parseFloat(prompt("enter second number"))
+// var opeater=prompt("enter operater +,_,*,/")
 
-switch(opeater){
-    case "+":
-        result=firstName+secondName
-        console.log("addition of first number and seconde num is "+ result)
-        break;
-        case "_":
-            result=firstName-secondName
-            console.log("subtraction  of first number and seconde num is" - result)
-            break;
-            case "*":
-                result=firstName*secondName
-                console.log("multiplication of first number and seconde number is " *result)
-                break;
-                case"/":
-                   result=firstName/secondName
-                console.log("division  of first number and seconde number is " /result)
-                break;
+// switch(opeater){
+//     case "+":
+//         result=firstName+secondName
+//         console.log("addition of first number and seconde num is "+ result)
+//         break;
+//         case "_":
+//             result=firstName-secondName
+//             console.log("subtraction  of first number and seconde num is" - result)
+//             break;
+//             case "*":
+//                 result=firstName*secondName
+//                 console.log("multiplication of first number and seconde number is " *result)
+//                 break;
+//                 case"/":
+//                    result=firstName/secondName
+//                 console.log("division  of first number and seconde number is " /result)
+//                 break;
+
+// }
+// loop
+var i=0
+while(i<5){
+    console.log("the value of i is " )
+    i++
+
+}
+// steps of while loop
+// initilization
+// condition
+// execution
+// icrement/dectement
+
+// doo loop
+// increment
+var i=0
+do{
+    console.log(("value of i in do is ") )
+    i++
+}while(i<5)
+
+// decrement
+var i=5
+do{
+    console.log("the value of u in do while loop and decrement zz" ,i)
+    i--
+}while(i>0)
+
+    // for loop steps
+    // initilization
+    // excetution
+    // icrement/decrement
+    // condition
+
+
+    
+// for 
+for(var i=0;i<2;i++){
+
+    console.log("the value of i in for loop is " ,i)
+}
+
+
+// decremenet of for loop
+for(var i=10;i>0;i--){
+    console.log("the value of i in for loop decrement ")
 
 }
